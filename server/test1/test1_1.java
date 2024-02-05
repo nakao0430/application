7f5cdac7a7e09ae6e7c726c0ca6d9d577df46e29
nakao0430 <nakao.taiki04@gmail.com>
2024-02-06 03:41:50 +0900

    server追加

---
+++ server/test1/test1_1.java
@@ -0,0 +1,13 @@
+package A;
+
+public class test1_1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.print("Hello World\n");
+        System.out.printf("Hello World");
+        // 追記テスト
+        System.out.printf("Hello Next World!");
+
+        // 解消確認用コメント
+    }
+}
---
+++ server/test1/test1_2.java
@@ -0,0 +1,9 @@
+package A;
+
+public class test1_2 {
+
+    public static void main(String[] args) {
+        int x = 11;
+        System.out.println("x=" + x);
+    }
+}
